531454b3e92f7b8ff36d957d1f99924c249fe762
Mvthxs <matheuscruvinel@hotmail.com>
2023-07-04 13:56:44 -0300

    intinerary function

--- app/services/openai_service.rb
+++ app/services/openai_service.rb
@@ -1,29 +1,31 @@
 require 'openai'
 require 'dotenv/load'
+require 'dotenv'
+
+Dotenv.load
 
 class OpenaiService
   def initialize
-    api_key = ENV['OPENAI_KEY']
-    @openai = OpenAI::Client.new(api_key: api_key)
+    api_key = ENV['OPENAI_API_KEY']
+    @openai = OpenAI::Client.new(access_token: api_key)
   end
 
-  def destination(destination)
-    @destination = destination
-  end
+   def destination(destination)
+     @destination = destination
+   end
 
-  def get_destination_info(destination)
-    prompt = "Irei viajar para #{destination}, me traga as informações relevantes sobre o local."
-    response = @openai.completions.create_engine("davinci-codex").prompt(prompt).generate(n: 1, stop: nil, temperature: 0.5)
-    destination_info = response.choices.first.text.strip
-
-    return { destination_info: destination_info }
-  end
-
-  def get_activity_suggestions(destination)
-    prompt = "Estarei viajando para #{destination}, traga me um roteiro de atividades para turismo."
-    response = @openai.completions.create_engine("davinci-codex").prompt(prompt).generate(n: 5, stop: nil, temperature: 0.7)
-    activity_suggestions = response.choices.map(&:text).map(&:strip)
-
-    return { activity_suggestions: activity_suggestions }
-  end
+   def get_travel_itinerary(destination, dias)
+    prompt = "Irei viajar para #{destination}, me traga as informações relevantes sobre o local e me faça roteiro de turismo para #{dias}."
+    response = @openai.chat(
+    parameters: {
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "user",
+          content: prompt
+        }
+      ], 
+      }
+    )
+    puts response.dig("choices", 0, "message", "content")
+   end
 end
