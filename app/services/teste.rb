13432f917116e4202625987efc8db3f20c57f3b3
Mvthxs <matheuscruvinel@hotmail.com>
2023-07-04 23:21:35 -0300

    teste.rb with weather info

--- app/services/teste.rb
+++ app/services/teste.rb
@@ -2,11 +2,15 @@
 
 require 'openai'
 require 'dotenv/load'
-require_relative 'openai_service'
 require 'dotenv'
+
+require_relative 'openai_service'
+require_relative 'weather_service'
 
 Dotenv.load
 
 openai_service = OpenaiService.new
+weather_service = WeatherService.new
 
-tourism_guide = openai_service.get_travel_itinerary("Paris", "3")
+tourism_guide = openai_service.get_travel_itinerary("Paris", "France", "3")
+weather_data = weather_service.get_weather_by_city("Paris", "France")
