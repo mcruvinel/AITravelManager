f48d0e6489fd359cb3a4be47aed8bfc34c5fd9dd
Mvthxs <matheuscruvinel@hotmail.com>
2023-07-05 16:45:59 -0300

    weather_service fixes

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -6,13 +6,16 @@ class WeatherService
   def get_weather_by_city(city, country)
     location_url = "https://api.weatherbit.io/v2.0/forecast/daily&city=#{city}&country=#{country}&key=#{WEATHERBIT_API_KEY}"
     location_response = HTTParty.get(location_url)
-    location_data = JSON.parse(location_response.body) if !location_response.empty?
+    if location_response.empty?
+      raise "No connection to the Weatherbit API."
+    end
+    location_data = JSON.parse(location_response.body)
 
     if location_data["status_code"] == 429
       raise "You have exceeded your API request limit."
     end
 
-    if location_data["status_code"] == 200 && location_data['data'].empty?
+    if location_data["status_code"] == 200 && !location_data['data'].empty?
       raise "No locations found for the city '#{city}'."
     end
     @weather_data = location_data['data'][0]
